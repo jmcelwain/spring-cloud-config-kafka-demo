952bd497cdb8c1313c24c659ad7ad9706a9344a0
James McElwain <jmcelwain@gmail.com>
2018-04-13 22:11:53 -0500

    pom structure

---
+++ client/src/main/kotlin/com/demo/springcloudconfigkafkademoclient/SpringCloudConfigKafkaDemoClientApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.springcloudconfigkafkademoclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudConfigKafkaDemoClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudConfigKafkaDemoClientApplication>(*args)
+}
---
+++ server/src/main/kotlin/com/demo/springcloudconfigkafkademoserver/SpringCloudConfigKafkaDemoServerApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.springcloudconfigkafkademoserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudConfigKafkaDemoServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudConfigKafkaDemoServerApplication>(*args)
+}
